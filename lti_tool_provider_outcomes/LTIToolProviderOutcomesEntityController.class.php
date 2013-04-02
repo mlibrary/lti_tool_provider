64a8989c22474352ff7b972bfd19487b60ccafc3
John.Zornig <j.zornig@uq.edu.au>
2013-04-02 10:57:38 +1000

    Refactor memberships to use batch api.

--- lti_tool_provider_outcomes/LTIToolProviderOutcomesEntityController.class.php
+++ lti_tool_provider_outcomes/LTIToolProviderOutcomesEntityController.class.php
@@ -1,77 +1,60 @@
 <?php
-
 /**
  * @file
- * Entity controller class for outcomes.
+ * Contains LTIToolProviderMembershipsContextEntityController.
  */
 
-interface LTIToolProviderOutcomesEntityControllerInterface
+/**
+ * Memberships Context Entity controller interface.
+ */
+interface LTIToolProviderMembershipsContextEntityControllerInterface
 extends DrupalEntityControllerInterface {
-
-  /**
-   * Create.
-   */
   public function create();
-
-  /**
-   * Save.
-   *
-   * @param object $entity
-   *   The Outcome to save.
-   */
   public function save($entity);
-
-  /**
-   * Delete.
-   *
-   * @param object $entity
-   *   The Outcome to delete.
-   */
   public function delete($entity);
 }
 
-class LTIToolProviderOutcomesEntityController
+/**
+ * Memberships Context Entity controller.
+ */
+class LTIToolProviderMembershipsContextEntityController
 extends DrupalDefaultEntityController
-implements LTIToolProvideroutcomesEntityControllerInterface {
-
-  /**
-   * @see LTIToolProviderOutcomesEntityControllerInterface::create
-   */
+implements LTIToolProviderMembershipsContextEntityControllerInterface {
   public function create() {
     $entity = new stdClass();
-    $entity->lti_tool_provider_outcomes_id = 0;
-    $entity->lti_tool_provider_outcomes_resource_entity_id_fk = 0;
-    $entity->lti_tool_provider_outcomes_result_sourcedid = '';
-    $entity->lti_tool_provider_outcomes_context_id = '';
-    $entity->lti_tool_provider_outcomes_user_id = '';
-    $entity->lti_tool_provider_outcomes_score = '';
+    $entity->lti_tool_provider_memberships_context_id = 0;
+    $entity->lti_tool_provider_memberships_context_context_id = NULL;
+    $entity->lti_tool_provider_memberships_context_file_id = 0;
     return $entity;
   }
 
   /**
-   * @see LTIToolProviderOutcomesEntityControllerInterface::save
+   * Save a memberships context entity.
+   *
+   * @param object $entity
+   *   The memberships context to be saved.
+   *
+   * @return object|boolean
+   *   The saved entity or FALSE.
    */
   public function save($entity) {
     $transaction = db_transaction();
-    try{
-      $entity->is_new = empty($enity->lti_tool_provider_outcomes_id);
-      if (empty($entity->lti_tool_provider_outcomes_date_joined)) {
-        $entity->lti_tool_provider_outcomes_date_joined = REQUEST_TIME;
-      }
-      field_attach_presave('lti_tool_provider_outcomes', $entity);
-      $primary_key = $entity->lti_tool_provider_outcomes_id ? 'lti_tool_provider_outcomes_id' : array();
+    try {
+      $entity->is_new = empty($entity->lti_tool_provider_memberships_context_id);
+      field_attach_presave('lti_tool_provider_memberships_context', $entity);
+      $primary_key = $entity->lti_tool_provider_memberships_context_id ? 'lti_tool_provider_memberships_context_id' : array();
       if (empty($primary_key)) {
-        drupal_write_record('lti_tool_provider_outcomes', $entity);
-        field_attach_insert('lti_tool_provider_outcomes', $entity);
+        drupal_write_record('lti_tool_provider_memberships_context', $entity);
+        field_attach_insert('lti_tool_provider_memberships_context', $entity);
         $op = 'insert';
       }
       else {
-        drupal_write_record('lti_tool_provider_outcomes', $entity, $primary_key);
+        drupal_write_record('lti_tool_provider_memberships_context', $entity, $primary_key);
         $op = 'update';
       }
       $function = 'field_attach_' . $op;
-      $function('lti_tool_provider_outcomes', $entity);
-      module_invoke_all('entity_' . $op, $entity, 'lti_tool_provider_outcomes');
+      $function('lti_tool_provider_memberships_context', $entity);
+      module_invoke_all('entity_' . $op, $entity, 'lti_tool_provider_memberships_context');
       unset($entity->is_new);
       db_ignore_slave();
       return $entity;
@@ -79,45 +62,45 @@ implements LTIToolProvideroutcomesEntityControllerInterface {
     catch (Exception $e) {
       $transaction->rollback();
       drupal_set_message(t('%e', array('%e' => $entity->$e)));
-      watchdog_exception('lti_tool_provider_outcomes', $e, NULL, WATCHDOG_ERROR);
+      watchdog_exception('lti_tool_provider_memberships_context', $e, NULL, WATCHDOG_ERROR);
       return FALSE;
     }
   }
 
   /**
-   * @see LTIToolProviderOutcomesEntityControllerInterface::delete
+   * Delete a memberships context entity.
+   *
+   * @param object $entity
+   *   The memberships context to be deleted.
    */
   public function delete($entity) {
-    $this->deleteMultiple(array($entity));
+    $this->delete_multiple(array($entity));
   }
 
   /**
-   * Delete Outcomes.
+   * Delete a list of memberships context entities.
    *
-   * @param array $entities
-   *   An array of Outcomes entities to delete.
-   *
-   * @throws Exception
+   * @param object $entities
+   *   An array of memberships contexts to be deleted.
    */
-  public function deleteMultiple($entities) {
+  public function delete_multiple($entities) {
     $ids = array();
     if (!empty($entities)) {
       $transaction = db_transaction();
       try {
         foreach ($entities as $entity) {
-          module_invoke_all('lti_tool_provider_outcomes_delete', $entity);
-          // Invoke hook_entity_delete().
-          module_invoke_all('entity_delete', $entity, 'lti_tool_provider_outcomes');
-          field_attach_delete('lti_tool_provider_outcomes', $entity);
-          $ids[] = $entity->lti_tool_provider_outcomes_id;
+          module_invoke_all('lti_tool_provider_memberships_context_delete', $entity);
+          module_invoke_all('entity_delete', $entity, 'lti_tool_provider_memberships_context');
+          field_attach_delete('lti_tool_provider_memberships_context', $entity);
+          $ids[] = $entity->lti_tool_provider_memberships_context_id;
         }
-        db_delete('lti_tool_provider_outcomes')
-        ->condition('lti_tool_provider_outcomes_id', $ids, 'IN')
+        db_delete('lti_tool_provider_memberships_context')
+        ->condition('lti_tool_provider_memberships_context_id', $ids, 'IN')
         ->execute();
       }
       catch (Exception $e) {
         $transaction->rollback();
-        watchdog_exception('lti_tool_provider_outcomes', $e);
+        watchdog_exception('lti_tool_provider_memberships_context', $e);
         throw $e;
       }
     }
